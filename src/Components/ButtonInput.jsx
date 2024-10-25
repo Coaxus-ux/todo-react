daaf796ad5c6619f1ab86ce0f77f99bac1367376
Coaxus-ux <trujillojulian.024@gmail.com>
2024-10-24 19:21:53 -0500

    icon stash times

--- src/Components/ButtonInput.jsx
+++ src/Components/ButtonInput.jsx
@@ -1,11 +1,8 @@
 export default function ButtonInput() {
-  const handleClick = () => {
-    console.log('clicked')
-  }
   return (
     <div className="button-container">
       <input type="text" placeholder="Add a task" />
-      <button onClick={handleClick} className="button-input">x</button>
+      <button className="button-input">x</button>
     </div>
   )
 }
